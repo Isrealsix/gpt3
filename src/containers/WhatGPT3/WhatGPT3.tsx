50bb9dba316ce20c31e0eab562f8fee36b0da125
Isrealsix's Macbook pro <isrealsix@gmail.com>
2022-03-28 16:19:35 +0700

    rewired with mixin

--- src/containers/WhatGPT3/WhatGPT3.tsx
+++ src/containers/WhatGPT3/WhatGPT3.tsx
@@ -1,8 +1,22 @@
+import { Feature } from '../../components';
 import './WhatGPT3.scss';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className="whatgpt3 section__margin" id="whatgpt3">
+      <div className="whatgpt3__features">
+        <Feature />
+      </div>
+      <div className="whatgpt3__heading">
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="whatgpt3__container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
