570f3acb0d4642c78a32e5882b02853520b40124
Isrealsix's Macbook pro <isrealsix@gmail.com>
2022-03-29 21:00:32 +0700

    completed the blog section

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -2,14 +2,21 @@ import './Article.scss';
 
 type Props = {
   imgUrl: string;
+  date: string;
+  title: string;
 }
 
-const Article:React.FC<Props> = ({ imgUrl }) => {
+const Article:React.FC<Props> = ({ imgUrl, date, title }) => {
   return (
     <div className="article">
       <div className="article__image">
         <img src={imgUrl} alt="blog" />
       </div>
+      <div className="article__content">
+        <p>{date}</p>
+        <h3>{title}</h3>  
+      </div>
+      <p>Read Full Article</p>
     </div>
   )
 }
