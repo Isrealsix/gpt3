317be8f315bffc2d55b0267016289f1b5dda5d2b
Isrealsix's Macbook pro <isrealsix@gmail.com>
2022-03-29 18:34:24 +0700

    completed the CTA section

--- src/components/Cta/Cta.tsx
+++ src/components/Cta/Cta.tsx
@@ -2,7 +2,15 @@ import './Cta.scss';
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className="cta">
+      <div className="cta__content">
+        <p>Request Early  Access to Get Started</p>
+        <h3>Register today & start exploring the endless possibilities</h3>
+      </div>
+      <div className="cta__button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
