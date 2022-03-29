a6d1b720c21d26b65e9b34016890b108aca25804
Isrealsix's Macbook pro <isrealsix@gmail.com>
2022-03-29 17:40:09 +0700

    completed the possibility section

--- src/containers/Possibility/Possibility.tsx
+++ src/containers/Possibility/Possibility.tsx
@@ -1,9 +1,27 @@
-import './Possibility.scss';
+import possibilityImage from "../../assets/possibility.png";
+import "./Possibility.scss";
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
-  )
-}
+    <div className="possibility section-padding" id="possibility">
+      <div className="possibility__image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="possibility__content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Exercitationem qui aspernatur, deleniti recusandae commodi, officiis,
+          reprehenderit veniam ducimus hic est temporibus neque autem cupiditate
+          incidunt non suscipit asperiores minus?
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
+};
 
-export default Possibility
+export default Possibility;
